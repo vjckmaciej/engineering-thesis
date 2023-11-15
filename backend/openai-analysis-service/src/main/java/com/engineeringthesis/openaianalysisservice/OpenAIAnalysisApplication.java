4ac80f6f9156cea204348cebe679b20ea118e405
vjckmaciej <macwojcik20@gmail.com>
2023-11-16 00:14:57 +0100

    Move OpenAIAnalysisApplication, add pom dependencies

--- backend/openai-analysis-service/src/main/java/com/engineeringthesis/openaianalysisservice/OpenAIAnalysisApplication.java
+++ backend/openai-analysis-service/src/main/java/com/engineeringthesis/openaianalysisservice/OpenAIAnalysisApplication.java
@@ -1,4 +1,4 @@
-package openaianalysisservice;
+package com.engineeringthesis.openaianalysisservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
