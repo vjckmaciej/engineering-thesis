2fa34bf4d36fe2930fae7d7c91d28b1179314fec
vjckmaciej <macwojcik20@gmail.com>
2023-11-26 01:53:59 +0100

    Init commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Button, Text} from "@mantine/core";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Hello world!</Button>
+    </div>
+  );
+}
+
+export default App;
